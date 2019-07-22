e9a1b62b2d3a90fb53d0d5b031564a198268cc62
Yoo <yooflyhigh@gmail.com>
2019-07-22 12:01:57 +0900

    navi 브랜치 적용

--- ClubManageMent/app/src/main/java/com/example/clubmanagement/System/Fragment/Fragment_Start.java
+++ ClubManageMent/app/src/main/java/com/example/clubmanagement/System/Fragment/Fragment_Start.java
@@ -39,8 +39,6 @@ public class Fragment_Start extends AppCompatActivity {
             for(Fragment fragment : fragmentList){
                 if(fragment instanceof OnBackPressedListener){
                     ((OnBackPressedListener)fragment).onBackPressed();
-                    //되나안되나 체크
-
                 }
             }
         }
